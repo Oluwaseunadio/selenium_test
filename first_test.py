f23490396a2e8a1e653bcbfc3a254db31522a458
Oluwaseunadio <oluwaseunadio31@gmail.com>
2025-03-28 22:06:42 +0100

    Saucedemo_website_assignment

--- first_test.py
+++ first_test.py
@@ -3,42 +3,46 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# variable declaration
+URL = "https://www.saucedemo.com/"
+Time = 2
 
+#initializing browser
 driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
+driver.get(URL)
 driver.maximize_window()
 
 # Fill the login details
 driver.find_element(By.ID, "user-name").send_keys("visual_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.ID, "login-button").click()
-time.sleep(2)
+time.sleep(Time)
 
 # Add products to carts
-driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
-driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light").click()
-driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
-driver.find_element(By.ID, "add-to-cart-sauce-labs-fleece-jacket").click()
-driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
-driver.find_element(By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)").click()
-driver.find_element(By.ID, "shopping_cart_container").click()
-time.sleep(2)
+Backpack = driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+BikeLight = driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light").click()
+BoltTShirt = driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
+FleeceJacket = driver.find_element(By.ID, "add-to-cart-sauce-labs-fleece-jacket").click()
+labsOniese = driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
+RedTshirt = driver.find_element(By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)").click()
+CartContainer = driver.find_element(By.ID, "shopping_cart_container").click()
+time.sleep(Time)
 
 # Proceed to checkout
 driver.find_element(By.ID, "checkout").click()
 
 # Fill customer information
-driver.find_element(By.ID, "first-name").send_keys("oluwaseun")
-driver.find_element(By.ID, "last-name").send_keys("Adio")
-driver.find_element(By.ID, "postal-code").send_keys("234")
-driver.find_element(By.ID, "continue").click()
-time.sleep(2)
+FirstName = driver.find_element(By.ID, "first-name").send_keys("oluwaseun")
+LastName = driver.find_element(By.ID, "last-name").send_keys("Adio")
+PostalCode = driver.find_element(By.ID, "postal-code").send_keys("234")
+Continuing = driver.find_element(By.ID, "continue").click()
+time.sleep(Time)
 
 # Complete order
 driver.find_element(By.ID, "finish").click()
-time.sleep(2)
+time.sleep(Time)
 
 driver.find_element(By.ID, "back-to-products").click()
-time.sleep(2)
+time.sleep(Time)
 
 
